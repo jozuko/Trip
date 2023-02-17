8fb971530fb726c338831027b83a1b7cebefbe25
rika.mori <rika.mori@rei-frontier.jp>
2023-02-17 17:13:53 +0900

    add firebase-auth(add google)

--- lib/view/signin/signin_page.dart
+++ lib/view/signin/signin_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:trip/util/colors.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 import 'package:trip/util/global.dart';
+import 'package:trip/view/signin/singin_bloc.dart';
 import 'package:trip/view/signin/singin_mail_bloc.dart';
 import 'package:trip/view/signin/singin_mail_page.dart';
-import 'package:trip/widget/button/square_rounded_button.dart';
 import 'package:trip/widget/title_bar.dart';
 
 ///
@@ -29,7 +29,13 @@ class _SignInState extends State<SignInPage> {
         _buildTitleBar(),
         Padding(
           padding: const EdgeInsets.all(margin),
-          child: _buildMailProvider(),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SignInButton(Buttons.email, onPressed: _onTapEmail),
+              SignInButton(Buttons.google, onPressed: _onTapGoogle),
+            ],
+          ),
         ),
       ],
     );
@@ -39,36 +45,7 @@ class _SignInState extends State<SignInPage> {
     return const TitleBar(title: '旅行プラン作成アプリ');
   }
 
-  Widget _buildMailProvider() {
-    return SquareRoundedButton(
-      onPressed: onTapMailProvider,
-      child: Center(
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: const [
-            Icon(
-              Icons.mail,
-              size: 40.0,
-              color: TColors.white,
-            ),
-            SizedBox(
-              width: marginS,
-            ),
-            Text(
-              'メールアドレスで認証',
-              style: TextStyle(
-                fontSize: fontSize3,
-                fontWeight: FontWeight.bold,
-                color: TColors.white,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void onTapMailProvider() {
+  void _onTapEmail() {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) => BlocProvider(
@@ -77,5 +54,9 @@ class _SignInState extends State<SignInPage> {
         ),
       ),
     );
+  }
+
+  void _onTapGoogle() {
+    BlocProvider.of<SignInBloc>(context).add(SignInGoogleEvent());
   }
 }
--- lib/view/signin/singin_bloc.dart
+++ lib/view/signin/singin_bloc.dart
@@ -1,12 +1,24 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trip/service/auth_service.dart';
+import 'package:trip/util/global.dart';
 
 ///
 /// Created by jozuko on 2023/02/17.
 /// Copyright (c) 2023 Studio Jozu. All rights reserved.
 ///
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
-  SignInBloc() : super(SignInState());
+  SignInBloc() : super(const SignInState()) {
+    on<SignInGoogleEvent>(_onAuthGoogle);
+  }
+
+  Future<void> _onAuthGoogle(event, emit) async {
+    final authService = getIt.get<AuthService>();
+
+    emit(state.copyWith(isLoading: true));
+    final result = await authService.authWithGoogle();
+    emit(state.copyWith(isLoading: false));
+  }
 }
 
 abstract class SignInEvent extends Equatable {
@@ -14,7 +26,30 @@ abstract class SignInEvent extends Equatable {
   List<Object?> get props => [];
 }
 
+class SignInGoogleEvent extends SignInEvent {}
+
 class SignInState extends Equatable {
+  final bool isLoading;
+  final bool isDoneAuth;
+
+  const SignInState({
+    this.isLoading = false,
+    this.isDoneAuth = false,
+  });
+
+  SignInState copyWith({
+    bool? isLoading,
+    bool? isDoneAuth,
+  }) {
+    return SignInState(
+      isLoading: isLoading ?? this.isLoading,
+      isDoneAuth: isDoneAuth ?? this.isDoneAuth,
+    );
+  }
+
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [
+        isLoading,
+        isDoneAuth,
+      ];
 }
